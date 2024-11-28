5b7d17c64604d44e7e466a96699ac9ba01f217e7
MED SALEM GHALASS <msghalas@gmail.com>
2024-11-28 12:06:51 +0000

    crud product

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -5,12 +5,62 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\Product;
 
+
 class Products extends Component
 {
-    public $products;
+    public $products, $description, $quantity, $product_id;
+    public $modal = false;
+
     public function render()
     {
         $this->products = Product::all();
         return view('livewire.products');
+    }
+
+    function create()
+    {
+        $this->clearFields(); // limpiarCampos
+        $this->openModal(); // abrirModal
+    }
+    function openModal() // abrirModal
+    {
+        $this->modal = true;
+    }
+    function closeModal() //cerrarModal
+    {
+        $this->modal = false;
+    }
+    function clearFields()
+    {
+        $this->description = '';
+        $this->quantity = '';
+        $this->product_id = '';
+    }
+    function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        $this->product_id = $id;
+        $this->description = $product->description;
+        $this->quantity = $product->quantity;
+        $this->openModal();
+    }
+    function delete($id) // borrar
+    {
+        Product::find($id)->delete();
+    }
+    function save() // guardar
+    {
+        $this->validate([
+            'description' => 'required',
+            'quantity' => 'required|integer|min:0'
+        ]);
+
+        Product::updateOrCreate(['id' => $this->product_id], [
+            'description' => $this->description,
+            'quantity' => $this->quantity,
+        ]);
+
+        $this->closeModal();
+        $this->clearFields();
     }
 }
--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -1,20 +1,50 @@
-<div>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>DESCRIPTION</th>
-                <th>QUANTITY</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($products as $product)
-                <tr>
-                    <td>{{ $product->id }}</td>
-                    <td>{{ $product->description }}</td>
-                    <td>{{ $product->quantity }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<x-slot name="header">
+    <h1 class="text-center">CRUD on LARAVEL and LIVEWIRE</h1>
+</x-slot>
+
+<div class="py-2">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+
+            <button wire:click='create()'
+                class="border border-green-500 text-green-500 hover:bg-green-600 hover:text-white  font-bold py-1 px-3 mb-2 rounded-md">
+                New
+            </button>
+
+            @if ($modal)
+                @include('livewire.create')
+            @endif
+
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-gray-500 text-white">
+                        <th class="px-4 py-1">ID</th>
+                        <th class="px-4 py-1">DESCRIPTION</th>
+                        <th class="px-4 py-1">QUANTITY</th>
+                        <th class="px-4 py-1">ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td class="border px-4 py-1">{{ $product->id }}</td>
+                            <td class="border px-4 py-1">{{ $product->description }}</td>
+                            <td class="border px-4 py-1">{{ $product->quantity }}</td>
+                            <td class="border px-4 py-1 text-center">
+                                <button wire:click='edit({{ $product->id }})'
+                                    class="border border-blue-500 text-blue-500 hover:bg-blue-600 hover:text-white py-1 px-2 rounded-md">
+                                    Edit
+                                </button>
+                                <button wire:click='delete({{ $product->id }})'
+                                    class="border border-red-500 text-red-500 hover:bg-red-600 hover:text-white py-1 px-2 rounded-md">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+    </div>
 </div>
